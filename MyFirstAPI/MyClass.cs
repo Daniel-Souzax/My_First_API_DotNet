329f009dc0af87093351a943ac9929dd757c94c4
Daniel S. Filho <danieldsfilho.dev@gmail.com>
2024-05-06 23:17:18 -0300

    feat: learning about appsetings.json

---
+++ MyFirstAPI/MyClass.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstAPI;
+
+public class MyClass
+{
+    public string PropA { get; set; }
+    public string Prop1 { get; set; }
+    public string Prop2 { get; set; }
+    public int Number {  get; set; }
+}
