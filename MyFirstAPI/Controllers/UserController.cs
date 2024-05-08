567b930d811b314dd981c5f58bc9a9a7d8db9d02
Daniel S. Filho <danieldsfilho.dev@gmail.com>
2024-05-08 16:59:21 -0300

    feat create new endpoints

---
+++ MyFirstAPI/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Communication.Requests;
+using MyFirstAPI.Communication.Responses;
+
+namespace MyFirstAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    
+    public IActionResult Get([FromHeader]int id, [FromHeader]string? nickname) 
+    {
+        var response = new User
+        {
+            Id = 1,
+            Age = 22,
+            Name = "Dan"
+        };
+
+        return Ok(response);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+    public IActionResult Create([FromBody] RequestRegisterUserJson request)
+    {
+        var response = new ResponseRegisterUserJson
+        {
+            Id = 1,
+            Name = request.Name
+        };
+
+        return Created(string.Empty, response);
+    }
+}
+    
