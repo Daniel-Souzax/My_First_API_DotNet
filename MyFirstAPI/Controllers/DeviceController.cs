ca0f3aea28b908a557536adfe6fcc464a9b8812c
Daniel S. Filho <danieldsfilho.dev@gmail.com>
2024-05-14 17:29:58 -0300

    feat: add new controller and new entities

---
+++ MyFirstAPI/Controllers/DeviceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Entities;
+
+namespace MyFirstAPI.Controllers;
+
+public class DeviceController : MyFirstApiBaseController
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var laptop = new Laptop();
+
+        var model = laptop.GetModel();  
+
+        return Ok(model);
+    }
+}
